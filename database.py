0ee73fb52df68c789c3e2a59337ab1dc044d6dbb
Davron <152484311+Davron030901@users.noreply.github.com>
2024-04-07 10:39:50 -0700

    Update database.py

--- database.py
+++ database.py
@@ -1,8 +1,17 @@
 import sqlalchemy
 
-engine=sqlalchemy.create_engine('sqlite:///movies.db',echo=True)
+engine= sqlalchemy.create_engine('sqlite:///users.db',echo=True)
+
+users_to_insert=[{'first_name':'Sarah','last_name':'Pery','email_address':'sarah.perry@gmail.com'},{'first_name':'Felix','last_name':'Martin','email_address':'felix.martin@gmail.com'},{'first_name':'John','last_name':'Patrick','email_address':'john.patrick@gmail'},{'first_name':'Jessica','last_name':'Jones','email_address':'jessica.jones@gmail.com'},{'first_name':'Percy','last_name':'Colton','email_address':'percy.colton@gmail.com'}]
+
+metadata=sqlalchemy.MetaData()
+
+users_table=sqlalchemy.Table('users',metadata,sqlalchemy.Column('user_id',sqlalchemy.Integer,primary_key=True),sqlalchemy.Column('first_name',sqlalchemy.String),sqlalchemy.Column('last_name',sqlalchemy.String),sqlalchemy.Column('email_address',sqlalchemy.String))
+metadata.create_all(engine)
 
 with engine.connect() as conn:
-    result=conn.execute(sqlalchemy.text("SELECT * FROM Movies"))
-    for row in result:
+    conn.execute(sqlalchemy.insert(users_table).values(users_to_insert))
+    for row in conn.execute(sqlalchemy.select(users_table)):
         print(row)
+
+    conn.commit()
